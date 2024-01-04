2d024084e99aa90a1207bbd65b59aba02fc935dd
Bensas <juanbensa@gmail.com>
2024-01-04 17:03:38 -0300

    Fixed the way messaged were being sent in simulation utlilty

--- simulation-utility/src/main.rs
+++ simulation-utility/src/main.rs
@@ -1,9 +1,17 @@
 use std::{time::{Duration, self}, thread};
 
-const BASE_OBEC_STATUS_DATA_MSG = "CM,0,001410.125.24.12\0";
-const OBEC_STATUS_DATA_HIGH_PRESSURE_MSG = "CM,2,010910.125.24.12\0";
 
-const EXT_VENT_AS_DEFAULT_MSG = "CM,2,EV0";
+// TODO: Create a command builder or simulation control class to build theses strings in a nicer manner
+const COMMUNICATION_MODULE_OBEC_STATUS_AVAILABLE_TRUE = "CM,3,1";
+const COMMUNICATION_MODULE_OBEC_STATUS_AVAILABLE_FALSE = "CM,3,0";
+const COMMUNICATION_MODULE_OBEC_STATUS_BASE = "CM,0,001410.125.24.12\0";
+const COMMUNICATION_MODULE_OBEC_STATUS_HIGH_PRESSURE = "CM,2,010910.125.24.12\0";
+
+const COMMUNICATION_MODULE_MCC_COMMAND_AVAILABLE_TRUE = "CM,2,1";
+const COMMUNICATION_MODULE_MCC_COMMAND_AVAILABLE_FALSE = "CM,2,0";
+
+const COMMUNICATION_MODULE_MCC_COMMAND_EXT_VENT_AS_DEFAULT_FALSE = "CM,0,EV0";
+const COMMUNICATION_MODULE_MCC_COMMAND_EXT_VENT_AS_DEFAULT_TRUE = "CM,0,EV1";
 
 fn main() {
     let write_port_name = "/dev/ttys003"; // Replace with the correct port
@@ -21,38 +29,48 @@ fn main() {
 }
 
 fn pressure_abort(&mut write_port) {
-    write_port.write_all(&BASE_OBEC_STATUS_DATA_MSG);
+    write_port.write_all(&COMMUNICATION_MODULE_OBEC_STATUS_BASE);
     thread::sleep(time::Duration::from_millis(1000));
-    write_port.write_all(&OBEC_STATUS_DATA_HIGH_PRESSURE_MSG);
+    write_port.write_all(&COMMUNICATION_MODULE_OBEC_STATUS_HIGH_PRESSURE);
     
     write_port.close().expect("Failed to close write port");
 }
 
 fn pressure_warning_and_back_to_standby(&mut write_port) {
     // We shoud also write the value of OBEC_status_data_available, right? Before or after writing the value of OBECStatus_data?
-    write_port.write_all(&BASE_OBEC_STATUS_DATA_MSG);
+    write_port.write_all(&COMMUNICATION_MODULE_OBEC_STATUS_BASE);
+    write_port.write_all(&COMMUNICATION_MODULE_OBEC_STATUS_AVAILABLE_TRUE);
     thread::sleep(time::Duration::from_millis(1000));
-    write_port.write_all(&OBEC_STATUS_DATA_HIGH_PRESSURE_MSG);
+    write_port.write_all(&COMMUNICATION_MODULE_OBEC_STATUS_HIGH_PRESSURE);
+    write_port.write_all(&COMMUNICATION_MODULE_OBEC_STATUS_AVAILABLE_TRUE);
     thread::sleep(time::Duration::from_millis(5000));
-    write_port.write_all(&BASE_OBEC_STATUS_DATA_MSG);
+    write_port.write_all(&COMMUNICATION_MODULE_OBEC_STATUS_BASE);
+    write_port.write_all(&COMMUNICATION_MODULE_OBEC_STATUS_AVAILABLE_TRUE);
 
     write_port.close().expect("Failed to close write port");
 }
 
 fn pressure_warning_ext_vent_and_back_to_standby_plus_ext_vent_as_default_check(&mut write_port) {
-    write_port.write_all(&BASE_OBEC_STATUS_DATA_MSG);
+    write_port.write_all(&COMMUNICATION_MODULE_OBEC_STATUS_BASE);
+    write_port.write_all(&COMMUNICATION_MODULE_OBEC_STATUS_AVAILABLE_TRUE);
     thread::sleep(time::Duration::from_millis(1000));
-    write_port.write_all(&OBEC_STATUS_DATA_HIGH_PRESSURE_MSG);
+    write_port.write_all(&COMMUNICATION_MODULE_OBEC_STATUS_HIGH_PRESSURE);
+    write_port.write_all(&COMMUNICATION_MODULE_OBEC_STATUS_AVAILABLE_TRUE);
     thread::sleep(time::Duration::from_millis(25000));
-    write_port.write_all(&BASE_OBEC_STATUS_DATA_MSG);
+    write_port.write_all(&COMMUNICATION_MODULE_OBEC_STATUS_BASE);
+    write_port.write_all(&COMMUNICATION_MODULE_OBEC_STATUS_AVAILABLE_TRUE);
     thread::sleep(time::Duration::from_millis(1500));
-    write_port.write_all(&OBEC_STATUS_DATA_HIGH_PRESSURE_MSG);
+    write_port.write_all(&COMMUNICATION_MODULE_OBEC_STATUS_HIGH_PRESSURE);
+    write_port.write_all(&COMMUNICATION_MODULE_OBEC_STATUS_AVAILABLE_TRUE);
     thread::sleep(time::Duration::from_millis(5000));
-    write_port.write_all(&BASE_OBEC_STATUS_DATA_MSG);
+    write_port.write_all(&COMMUNICATION_MODULE_OBEC_STATUS_BASE);
+    write_port.write_all(&COMMUNICATION_MODULE_OBEC_STATUS_AVAILABLE_TRUE);
     thread::sleep(time::Duration::from_millis(1000));
     write_port.write_all(&EXT_VENT_AS_DEFAULT_MSG);
+    write_port.write_all(&COMMUNICATION_MODULE_OBEC_STATUS_AVAILABLE_TRUE);
     thread::sleep(time::Duration::from_millis(1000));
-    write_port.write_all(&OBEC_STATUS_DATA_HIGH_PRESSURE_MSG);
+    write_port.write_all(&COMMUNICATION_MODULE_OBEC_STATUS_HIGH_PRESSURE);
+    write_port.write_all(&COMMUNICATION_MODULE_OBEC_STATUS_AVAILABLE_TRUE);
     
     write_port.close().expect("Failed to close write port");
 }
