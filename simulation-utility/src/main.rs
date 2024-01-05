992ce44eadd1aefb9def6497b4a2f2eef95c287d
Bensas <juanbensa@gmail.com>
2024-01-05 10:13:22 -0300

    Implemented simulation code with simultaneous reading and writing from (and to) serial port using mutex and threads.

--- simulation-utility/src/main.rs
+++ simulation-utility/src/main.rs
@@ -1,4 +1,4 @@
-use std::{time::{Duration, self}, thread, borrow::Borrow, io::{self, BufRead}};
+use std::{time::{Duration, self}, thread, borrow::Borrow, io::{self, BufRead}, sync::{Arc, Mutex, MutexGuard}};
 
 use serialport::SerialPort;
 
@@ -18,22 +18,24 @@ const COMMUNICATION_MODULE_MCC_COMMAND_EXT_VENT_AS_DEFAULT_TRUE: &[u8] = b"CM,0,
 const COMMUNICATION_MODULE_MCC_COMMAND_SWITCH_STATE_LOADING: &[u8] = b"CM,0,SSLDNG";
 
 fn main() {
-    let write_port_name = "/dev/cu.usbserial-0001"; // Replace with the correct port
+    let serial_port_name = "/dev/cu.usbserial-0001"; // Replace with the correct port
     let baud_rate = 460800;
 
     // Open the write port
-    let mut write_port = serialport::new(write_port_name, baud_rate)
+    let mut serial_port = serialport::new(serial_port_name, baud_rate)
         .timeout(Duration::from_millis(600))
         .open()
         .expect("Failed to open write port");
 
-    let mut reader = io::BufReader::new(write_port);
-    let mut line = String::new();
+    let serial_port_arc = Arc::new(Mutex::new(serial_port));
 
+    let reader_port = Arc::clone(&serial_port_arc);
     thread::spawn(move || {
         println!("Spawned thread for reading serial port");
         loop {
-            let mut read_buf: Vec<u8> = vec![0; 400]; // Adjust the buffer size as needed
+            let mut port = reader_port.lock().unwrap();
+            let mut reader = io::BufReader::new(&mut *port);
+            let mut line = String::new();
             match reader.read_line(&mut line) {
                 Ok(bytes_read) => {
                     if bytes_read > 0 {
@@ -43,67 +45,75 @@ fn main() {
                 }
                 Err(e) => eprintln!("Error reading from serial port: {}", e),
             }
+            drop(port);
+            thread::sleep(time::Duration::from_millis(500));        
         }
     });
 
+    thread::sleep(time::Duration::from_millis(2000));
+
+    pressure_abort(serial_port_arc);
     loop {};
 
-    
-
-    // pressure_abort(write_port);
 }
 
-fn pressure_abort(mut write_port: Box<dyn SerialPort>) {
-    write_port.write_all(&COMMUNICATION_MODULE_OBEC_STATUS_BASE);
+fn pressure_abort(mut serial_port_mutex: Arc<Mutex<Box<dyn SerialPort>>>) {
+    let mut serial_port = serial_port_mutex.lock().unwrap();
+    serial_port.write_all(&COMMUNICATION_MODULE_OBEC_STATUS_BASE);
+    println!("Wrote to serial port once");
+    drop(serial_port);
     thread::sleep(time::Duration::from_millis(1000));
-    write_port.write_all(&COMMUNICATION_MODULE_OBEC_STATUS_HIGH_PRESSURE);
+    serial_port = serial_port_mutex.lock().unwrap();
+    serial_port.write_all(&COMMUNICATION_MODULE_OBEC_STATUS_HIGH_PRESSURE);
+    println!("Wrote to serial port again!");
+    drop(serial_port);
 }
 
-fn pressure_warning_and_back_to_standby(mut write_port: Box<dyn SerialPort>) {
+fn pressure_warning_and_back_to_standby(mut serial_port: Box<dyn SerialPort>) {
     // We shoud also write the value of OBEC_status_data_available, right? Before or after writing the value of OBECStatus_data?
-    write_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_BASE);
-    write_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_AVAILABLE_TRUE);
+    serial_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_BASE);
+    serial_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_AVAILABLE_TRUE);
     thread::sleep(time::Duration::from_millis(1000));
-    write_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_HIGH_PRESSURE);
-    write_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_AVAILABLE_TRUE);
+    serial_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_HIGH_PRESSURE);
+    serial_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_AVAILABLE_TRUE);
     thread::sleep(time::Duration::from_millis(5000));
-    write_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_BASE);
-    write_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_AVAILABLE_TRUE);
+    serial_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_BASE);
+    serial_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_AVAILABLE_TRUE);
 }
 
-fn pressure_warning_ext_vent_and_back_to_standby_plus_ext_vent_as_default_check(mut write_port: Box<dyn SerialPort>) {
-    write_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_BASE);
-    write_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_AVAILABLE_TRUE);
+fn pressure_warning_ext_vent_and_back_to_standby_plus_ext_vent_as_default_check(mut serial_port: Box<dyn SerialPort>) {
+    serial_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_BASE);
+    serial_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_AVAILABLE_TRUE);
     thread::sleep(time::Duration::from_millis(1000));
-    write_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_HIGH_PRESSURE);
-    write_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_AVAILABLE_TRUE);
+    serial_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_HIGH_PRESSURE);
+    serial_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_AVAILABLE_TRUE);
     thread::sleep(time::Duration::from_millis(25000));
-    write_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_BASE);
-    write_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_AVAILABLE_TRUE);
+    serial_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_BASE);
+    serial_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_AVAILABLE_TRUE);
     thread::sleep(time::Duration::from_millis(1500));
-    write_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_HIGH_PRESSURE);
-    write_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_AVAILABLE_TRUE);
+    serial_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_HIGH_PRESSURE);
+    serial_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_AVAILABLE_TRUE);
     thread::sleep(time::Duration::from_millis(5000));
-    write_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_BASE);
-    write_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_AVAILABLE_TRUE);
+    serial_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_BASE);
+    serial_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_AVAILABLE_TRUE);
     thread::sleep(time::Duration::from_millis(1000));
-    write_port.write_all(COMMUNICATION_MODULE_MCC_COMMAND_EXT_VENT_AS_DEFAULT_TRUE);
-    write_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_AVAILABLE_TRUE);
+    serial_port.write_all(COMMUNICATION_MODULE_MCC_COMMAND_EXT_VENT_AS_DEFAULT_TRUE);
+    serial_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_AVAILABLE_TRUE);
     thread::sleep(time::Duration::from_millis(1000));
-    write_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_HIGH_PRESSURE);
-    write_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_AVAILABLE_TRUE);
+    serial_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_HIGH_PRESSURE);
+    serial_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_AVAILABLE_TRUE);
     }
 
-fn loading_test(mut write_port: Box<dyn SerialPort>) {
-    write_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_BASE);
-    write_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_AVAILABLE_TRUE);
+fn loading_test(mut serial_port: Box<dyn SerialPort>) {
+    serial_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_BASE);
+    serial_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_AVAILABLE_TRUE);
     thread::sleep(time::Duration::from_millis(1000));
-    write_port.write_all(COMMUNICATION_MODULE_MCC_COMMAND_SWITCH_STATE_LOADING);
-    write_port.write_all(COMMUNICATION_MODULE_MCC_COMMAND_AVAILABLE_TRUE);
+    serial_port.write_all(COMMUNICATION_MODULE_MCC_COMMAND_SWITCH_STATE_LOADING);
+    serial_port.write_all(COMMUNICATION_MODULE_MCC_COMMAND_AVAILABLE_TRUE);
     thread::sleep(time::Duration::from_millis(1000));
-    write_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_LOW_TANK_VENT_TEMPERATURE);
-    write_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_AVAILABLE_TRUE);
+    serial_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_LOW_TANK_VENT_TEMPERATURE);
+    serial_port.write_all(COMMUNICATION_MODULE_OBEC_STATUS_AVAILABLE_TRUE);
     thread::sleep(time::Duration::from_millis(2000));
-    write_port.write_all(COMMUNICATION_MODULE_MCC_COMMAND_SWITCH_STATE_LOADING);
-    write_port.write_all(COMMUNICATION_MODULE_MCC_COMMAND_AVAILABLE_TRUE);
+    serial_port.write_all(COMMUNICATION_MODULE_MCC_COMMAND_SWITCH_STATE_LOADING);
+    serial_port.write_all(COMMUNICATION_MODULE_MCC_COMMAND_AVAILABLE_TRUE);
 }
